46ffb01a95a262d12a13255224c3c103dac0cbbd
Wlademyr Mendes <wlademyr.mendes@gmail.com>
2023-03-28 05:05:44 -0300

    Running local server with tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,11 +2,9 @@
 const timeout = process.env.SLOWMO ? 6000 : 4000;
 const fs = require('fs');
 
-// Go to the specified path and wait for the domcontent to load before running the tests
 beforeAll(async () => {
-  path = fs.realpathSync('index.html');
-  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
-});
+    await page.goto('http://localhost:8001');
+  });
 
 describe('Index tests', () => {
   test('check if the button acessar exists', async () => {
--- tests/title.test.js
+++ tests/title.test.js
@@ -2,10 +2,8 @@
 const timeout = process.env.SLOWMO ? 6000 : 4000;
 const fs = require('fs');
 
-// Go to the specified path and wait for the domcontent to load before running the tests
 beforeAll(async () => {
-  path = fs.realpathSync('index.html');
-  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+  await page.goto('http://localhost:8001');
 });
 
 describe('Title of the page', () => {
